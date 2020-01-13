02f4f926b545aad5a328dae188fa943db07c88fe
ChaoticFailer <xristosbarko@hotmail.com>
2020-01-13 18:34:01 +0200

    Updates

---
+++ precache-manifest.f00cd7ff08af49fe54fe2297093b4561.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f556a0d52049c33c8cbbd42dfb2f624",
+    "url": "/clientele_front_end/index.html"
+  },
+  {
+    "revision": "d5dad192ad0a71a2a74b",
+    "url": "/clientele_front_end/static/css/2.25b83785.chunk.css"
+  },
+  {
+    "revision": "7ab8f047d1afd6709cf2",
+    "url": "/clientele_front_end/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d5dad192ad0a71a2a74b",
+    "url": "/clientele_front_end/static/js/2.19439e5b.chunk.js"
+  },
+  {
+    "revision": "78768bc5cb5ec59e43df386590400beb",
+    "url": "/clientele_front_end/static/js/2.19439e5b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ab8f047d1afd6709cf2",
+    "url": "/clientele_front_end/static/js/main.c787a34a.chunk.js"
+  },
+  {
+    "revision": "7c235835118b7b7deeeb",
+    "url": "/clientele_front_end/static/js/runtime-main.0ff09513.js"
+  }
+]);
